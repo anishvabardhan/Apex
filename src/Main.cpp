4a799810c18e86980581451ac3c5be1c3e80e440
Anishva Bardhan <anishva.bardhan@gmail.com>
2021-02-13 16:46:40 +0530

    Setup Shader class Abstraction.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,80 +1,11 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
-
 #include "Graphics/Window.h"
 #include "Graphics/VertexBuffer.h"
 #include "Graphics/IndexBuffer.h"
 #include "Graphics/VertexArray.h"
 #include "Graphics/VertexBufferLayout.h"
+#include "Graphics/Shader.h"
 
 using namespace Apex;
-
-struct ShaderProgramSource
-{
-	std::string VertexSource;
-	std::string FragmentSource;
-};
-
-static ShaderProgramSource ParseShader(const std::string& filepath)
-{
-	std::ifstream stream(filepath);
-
-	enum class ShaderType
-	{
-		NONE = -1,
-		VERTEX = 0,
-		FRAGMENT = 1
-	};
-
-	std::string line;
-	std::stringstream ss[2];
-	ShaderType type = ShaderType::NONE;
-	while (getline(stream, line))
-	{
-		if (line.find("#shader") != std::string::npos)
-		{
-			if (line.find("vertex") != std::string::npos)
-				type = ShaderType::VERTEX;
-			else if (line.find("fragment") != std::string::npos)
-				type = ShaderType::FRAGMENT;
-		}
-		else
-		{
-			ss[(int)type] << line << '\n';
-		}
-	}
-
-	return { ss[0].str(), ss[1].str() };
-}
-
-static unsigned int CompileShader(unsigned int type, const std::string& source)
-{
-	unsigned int id = glCreateShader(type);
-	const char* src = source.c_str();
-	glShaderSource(id, 1, &src, nullptr);
-	glCompileShader(id);
-
-	return id;
-}
-
-static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
-{
-	unsigned int program = glCreateProgram();
-	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
-	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
-
-	glAttachShader(program, vs);
-	glAttachShader(program, fs);
-	glLinkProgram(program);
-	glValidateProgram(program);
-
-	glDeleteShader(vs);
-	glDeleteShader(fs);
-
-	return program;
-}
 
 int main()
 {
@@ -108,17 +39,15 @@ int main()
 		// Creating IndexBuffer
 		IndexBuffer ib(indices, 6);
 
-		ShaderProgramSource source = ParseShader("res/Shaders/Basic.shader");
-		unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-		glUseProgram(shader);
+		// Creating Shader
+		Shader shader("res/Shaders/Basic.shader");
+		shader.Bind();
+		shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
 
-		int location = glGetUniformLocation(shader, "u_Color");
-		glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f);
-
-		glBindVertexArray(0);
-		glUseProgram(0);
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		va.UnBind();
+		shader.UnBind();
+		vb.UnBind();
+		ib.UnBind();
 
 		// The Core Game Loop
 		while (!window->Close())
@@ -126,7 +55,7 @@ int main()
 			// Calling glClear()
 			window->Clear();
 
-			glUseProgram(shader);
+			shader.Bind();
 			va.Bind();
 			ib.Bind();
 
@@ -136,8 +65,6 @@ int main()
 			// Updating the Window every loop
 			window->Update();
 		}
-
-		glDeleteProgram(shader);
 	}
 	return 0;
 }
