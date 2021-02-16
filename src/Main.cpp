ae8d81ffa7afa080969f8d652c35e19f66a00c32
Anishva Bardhan <anishva.bardhan@gmail.com>
2021-02-16 14:52:47 +0530

    Updated the collision class with my own maths lib.

--- src/Main.cpp
+++ src/Main.cpp
@@ -12,18 +12,18 @@ using namespace Apex;
 
 int main()
 {
-	// Creating A Window--------------------------------------------------------------------------------------------
+// Creating A Window--------------------------------------------------------------------------------------------
 	Window* window;
 	window = new Window("Apex", 640, 480);
 	{
-		// Creating Shader------------------------------------------------------------------------------------------
+// Creating Shader------------------------------------------------------------------------------------------
 		Shader shader("res/Shaders/Basic.shader");
 		shader.Bind();
 
-		// Renderables
+// Renderables
 		Renderable2D sprite(Vec3(300, 200, 0), Vec2(100, 100), shader);
 
-		// Creating an orthographic camera--------------------------------------------------------------------------
+// Creating an orthographic camera--------------------------------------------------------------------------
 		//glm::mat4 proj = glm::ortho(0.0f, (float)window->GetWidth(), 0.0f, (float)window->GetHeight(), -1.0f, 1.0f);
 		//glm::mat4 model = glm::translate(glm::mat4(1.0f), sprite.GetPosition());
 		//glm::mat4 mvp = proj * model;
@@ -31,7 +31,7 @@ int main()
 		Mat4 proj = Mat4::orthographic(0.0f, (float)window->GetWidth(), 0.0f, (float)window->GetHeight(), -1.0f, 1.0f);
 		Mat4 model = Mat4::translation(sprite.GetPosition());
 
-		// Setting Uniforms-----------------------------------------------------------------------------------------
+// Setting Uniforms-----------------------------------------------------------------------------------------
 		shader.SetUniform4f("u_Color", 0.5f, 0.5f, 0.5f, 1.0f);
 		shader.SetUniformMat4f("proj", proj);
 		shader.SetUniformMat4f("model", model);
@@ -41,19 +41,19 @@ int main()
 		sprite.GetVBO()->UnBind();
 		shader.UnBind();
 
-		// Creating collision objects-------------------------------------------------------------------------------
+// Creating collision objects-------------------------------------------------------------------------------
 		//Collision2D collisionA, collisionB, collisionC;
 
-		// Creating the Renderer------------------------------------------------------------------------------------
+// Creating the Renderer------------------------------------------------------------------------------------
 		Renderer renderer;
 
 		// The Core Game Loop---------------------------------------------------------------------------------------
 		while (!window->Close())
 		{
-			// Calling glClear()------------------------------------------------------------------------------------
+// Calling glClear()------------------------------------------------------------------------------------
 			window->Clear();
 
-			// Rendering--------------------------------------------------------------------------------------------
+// Rendering--------------------------------------------------------------------------------------------
 			Mat4 model = Mat4::translation(sprite.GetPosition());
 			Mat4 mvp = proj * model;
 			
@@ -63,7 +63,7 @@ int main()
 			
 			renderer.Draw(sprite);
 
-			// Updating the Window every loop-----------------------------------------------------------------------
+// Updating the Window every loop-----------------------------------------------------------------------
 			window->Update();
 		}
 	}
--- src/Physics/2D/Collision2D.cpp
+++ src/Physics/2D/Collision2D.cpp
@@ -4,13 +4,13 @@ namespace Apex {
 
 	bool Collision2D::CollisionObjects(const Renderable2D& GameObj1, const Renderable2D& GameObj2)
 	{
-		if ((GameObj1.GetPosition().x + GameObj1.GetSize().x) == (GameObj2.GetPosition().x - GameObj2.GetSize().x) || (GameObj1.GetPosition().x) == (GameObj2.GetPosition().x + GameObj2.GetSize().x))
+		if ((GameObj1.GetPosition().m_X + GameObj1.GetSize().m_X) == (GameObj2.GetPosition().m_X - GameObj2.GetSize().m_X) || (GameObj1.GetPosition().m_X) == (GameObj2.GetPosition().m_X + GameObj2.GetSize().m_X))
 		{
 			increamentX = -increamentX;
 			return true;
 		}
 
-		if ((GameObj1.GetPosition().y + GameObj1.GetSize().y) == (GameObj2.GetPosition().y - GameObj2.GetSize().y) || (GameObj1.GetPosition().y) == (GameObj2.GetPosition().y + GameObj2.GetSize().y))
+		if ((GameObj1.GetPosition().m_Y + GameObj1.GetSize().m_Y) == (GameObj2.GetPosition().m_Y - GameObj2.GetSize().m_Y) || (GameObj1.GetPosition().m_Y) == (GameObj2.GetPosition().m_Y + GameObj2.GetSize().m_Y))
 		{
 			increamentY = -increamentY;
 			return true;
@@ -21,12 +21,12 @@ namespace Apex {
 
 	bool Collision2D::CollisionWorld(const Renderable2D& GameObj, const Window& window)
 	{
-		if (GameObj.GetPosition().x + GameObj.GetSize().x >= window.GetWidth() || GameObj.GetPosition().x <= 0)
+		if (GameObj.GetPosition().m_X + GameObj.GetSize().m_X >= window.GetWidth() || GameObj.GetPosition().m_X <= 0)
 		{
 			increamentX = -increamentX;
 			return true;
 		}
-		if (GameObj.GetPosition().y + GameObj.GetSize().y >= window.GetHeight() || GameObj.GetPosition().y <= 0)
+		if (GameObj.GetPosition().m_Y + GameObj.GetSize().m_Y >= window.GetHeight() || GameObj.GetPosition().m_Y <= 0)
 		{
 			increamentY = -increamentY;
 			return true;
