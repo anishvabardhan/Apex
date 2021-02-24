476dde9678eff0238cbafd3f55e29d73317be83a
Anishva Bardhan <anishva.bardhan@gmail.com>
2021-02-24 20:46:05 +0530

    Updated the uniforms in the shader.

--- res/Shaders/Basic.shader
+++ res/Shaders/Basic.shader
@@ -3,7 +3,6 @@
 
 layout(location = 0) in vec4 position;
 		
-//uniform mat4 proj;
 uniform mat4 u_ViewProj = mat4(1.0f);
 uniform mat4 u_Model = mat4(1.0f);
 
--- src/Tests/BreakOut/BreakOut.cpp
+++ src/Tests/BreakOut/BreakOut.cpp
@@ -36,7 +36,6 @@ void BreakOut::Init()
 //Setting Uniforms--------------------------------------------------------------------------------------------------------------------------------
 
 	shader.SetUniformMat4f("u_ViewProj", Camera.GetViewProjMatrix());
-	//shader.SetUniformMat4f("view", Camera.GetViewMatrix());
 
 // Creating a Collision object--------------------------------------------------------------------------------------------------------------------
 
@@ -66,7 +65,6 @@ void BreakOut::Init()
 
 		// Rendering the quad---------------------------------------------------------------------------------------------------------------------
 
-		
 		shader.SetUniform4f("u_Color", 1.0f, 0.0f, 0.0f, 1.0f);			
 		shader.SetUniformMat4f("u_Model", quad.GetModelPosition());
 
