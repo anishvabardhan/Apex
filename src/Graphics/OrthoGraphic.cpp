acd1273beca43b2dc76168807feaebb160b4caca
Anishva Bardhan <anishva.bardhan@gmail.com>
2021-02-25 20:37:43 +0530

    Setup a Transform matrix.

--- src/Graphics/OrthoGraphic.cpp
+++ src/Graphics/OrthoGraphic.cpp
@@ -8,4 +8,11 @@ namespace Apex {
 		m_ViewProj = m_Proj * m_View;
 	}
 
+	void OrthoGraphic::RecalculateViewMatrix()
+	{
+		Mat4 Transform = Mat4::translation(m_Position) * Mat4::rotation(m_Rotation, Vec3(0, 0, 1));
+
+		//m_View = 
+	}
+
 }
--- src/Graphics/OrthoGraphic.h
+++ src/Graphics/OrthoGraphic.h
@@ -7,11 +7,15 @@ namespace Apex {
 	class OrthoGraphic
 	{
 
-	// Member Variables-------------------------------------------------------------------------------------
+// Member Variables-------------------------------------------------------------------------------------
 
 		Mat4 m_Proj;
 		Mat4 m_View;
 		Mat4 m_ViewProj;
+
+		Vec3 m_Position;
+
+		float m_Rotation;
 	public:
 
 // Constructors---------------------------------------------------------------------------------------------
@@ -23,6 +27,14 @@ namespace Apex {
 		const Mat4& GetProjMatrix() const { return m_Proj; }
 		const Mat4& GetViewMatrix() const { return m_View; }
 		const Mat4& GetViewProjMatrix() const { return m_ViewProj; }
+
+		const Vec3 GetPosition() const { return m_Position; }
+		void SetPosition(const Vec3& position) { m_Position = position; RecalculateViewMatrix(); }
+
+		const float GetRotation() const { return m_Rotation; }
+		void SetRotation(const float& rotation) { m_Rotation = rotation; RecalculateViewMatrix(); }
+	private:
+		void RecalculateViewMatrix();
 	};
 
 }
