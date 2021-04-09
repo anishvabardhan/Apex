40266ede564e4815f0f794a79354c9e9dcd568be
Anishva Bardhan <anishva.bardhan@gmail.com>
2021-04-09 22:53:11 +0530

    Updated the code.

--- tests/Astroids/Ship.cpp
+++ tests/Astroids/Ship.cpp
@@ -18,11 +18,11 @@ namespace Apex {
 		glTranslatef(m_Position.m_X, m_Position.m_Y, 0.0f);
 
 		glBegin(GL_LINE_STRIP);
-		glColor3f(0.5f, 0.5f, 0.5f);
-		glVertex3f(0.0f, 100.0f, 0.0f);
-		glVertex3f(100.0f, -100.0f, 0.0f);
-		glVertex3f(-100.0f, -100.0f, 0.0f);
-		glVertex3f(0.0f, 100.0f, 0.0f);
+		glColor3f(   0.5f,   0.5f, 0.5f);
+		glVertex3f(  0.0f,  15.0f, 0.0f);
+		glVertex3f( 10.0f, -10.0f, 0.0f);
+		glVertex3f(-10.0f, -10.0f, 0.0f);
+		glVertex3f(  0.0f,  15.0f, 0.0f);
 		glEnd();
 	}
 
